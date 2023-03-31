4ad875779719028d912c187527fc378b0212999f
Roze <rozeu668@gmail.com>
2023-03-31 09:22:11 +0300

    Realizēta iespēja parādīt/paslēpt attēlus

---
+++ Assets/Scripts/AtteluParadisana.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AtteluParadisana : MonoBehaviour {
+    public GameObject Bean;
+    public GameObject Teddy;
+    public GameObject Grandma;
+    public GameObject Car;
+
+    public void BeanAttels(bool vertiba)
+    {
+        Bean.SetActive(vertiba);
+    }
+
+    public void TeddyAttels(bool vertiba)
+    {
+        Teddy.SetActive(vertiba);
+    }
+
+    public void GrandmaAttels(bool vertiba)
+    {
+        Grandma.SetActive(vertiba);
+    }
+
+    public void CarAttels(bool vertiba)
+    {
+        Car.SetActive(vertiba);
+    }
+}
