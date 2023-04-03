2fe09abf6157d53011cd71645e1aad89cf585ee3
RozeU <rozeu668@gmail.com>
2023-04-03 15:55:20 +0300

    Pabeigts darbs ar Toggle elementiem

--- Assets/Scripts/AtteluParadisana.cs
+++ Assets/Scripts/AtteluParadisana.cs
@@ -2,16 +2,21 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class AtteluParadisana : MonoBehaviour {
     public GameObject Bean;
     public GameObject Teddy;
     public GameObject Grandma;
     public GameObject Car;
+	public GameObject PaKreisi;
+	public GameObject PaLabi;
 
     public void BeanAttels(bool vertiba)
     {
         Bean.SetActive(vertiba);
+		PaKreisi.GetComponent<Toggle> ().interactable = vertiba;
+		PaLabi.GetComponent<Toggle> ().interactable = vertiba;
     }
 
     public void TeddyAttels(bool vertiba)
@@ -28,4 +33,12 @@ public class AtteluParadisana : MonoBehaviour {
     {
         Car.SetActive(vertiba);
     }
+
+	public void PaKreisiBean(){
+		Bean.transform.localScale = new Vector2 (1, 1);
+	}
+
+	public void PaLabiBean(){
+		Bean.transform.localScale = new Vector2 (-1, 1);
+	}
 }
